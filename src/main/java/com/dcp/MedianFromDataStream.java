f287049072db0e042948d57d76e8a1fa5dd481ba
Prabhakaran Nivanil <prabhakaran.nivanil@prod.hclpnp.com>
2020-06-12 16:23:15 +0530

    add debug in median from data stream

--- src/main/java/com/dcp/MedianFromDataStream.java
+++ src/main/java/com/dcp/MedianFromDataStream.java
@@ -18,12 +18,26 @@ public class MedianFromDataStream {
 
 		if (minHeap.size() < maxHeap.size())
 			minHeap.offer(maxHeap.poll());
+
 	}
 
 	public double findMedian() {
 		if (minHeap.size() > maxHeap.size())
 			return minHeap.peek();
 
-		return (minHeap.peek() + maxHeap.peek()) / 2;
+		return (minHeap.peek() + maxHeap.peek()) / 2.0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+		MedianFromDataStream obj = new MedianFromDataStream();
+
+		for (int i = 0; i < arr.length; i++) {
+			obj.addNum(arr[i]);
+			System.out.println(obj.minHeap);
+			System.out.println(obj.maxHeap);
+			System.out.println(obj.findMedian());
+		}
 	}
 }
