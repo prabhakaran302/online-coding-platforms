cc2c5641b30caf7f8d050291b4b7d0ccf7a59d89
Prabhakaran Nivanil <prabhakaran.nivanil@gmail.com>
2020-06-17 18:18:58 +0530

    delete incomplete files

---
+++ leetcode/com/practice/medium/FindAllAnagramsInString.java
@@ -0,0 +1,60 @@
+package com.practice.medium;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ * 
+ * @author prabhakaran.nivanil
+ *
+ */
+public class FindAllAnagramsInString {
+	public List<Integer> findAnagrams(String s, String p) {
+		List<Integer> list = new ArrayList<Integer>();
+		if (s.length() < p.length())
+			return list;
+
+		int sL = s.length();
+		int pL = p.length();
+
+		int[] sArray = new int[256];
+		int[] pArray = new int[256];
+
+		for (int i = 0; i < pL; i++) {
+			pArray[p.charAt(i)]++;
+			sArray[s.charAt(i)]++;
+		}
+
+		for (int i = pL; i < sL; i++) {
+			if (compare(sArray, pArray)) {
+				list.add(i - pL);
+			}
+			sArray[s.charAt(i)]++;
+
+			sArray[s.charAt(i - pL)]--;
+		}
+
+		/**
+		 * last values not included in previous loop
+		 */
+		if (compare(sArray, pArray)) {
+			list.add(sL - pL);
+		}
+		return list;
+	}
+
+	private boolean compare(int[] sArray, int[] pArray) {
+		for (int i = 0; i < sArray.length; i++) {
+			if (pArray[i] != sArray[i])
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new FindAllAnagramsInString().findAnagrams("abab", "ab"));
+	}
+
+}
